5f3743ac29cd621b581fdcd5b6c054dc3cf6d9df
+ <+>
2022-12-04 11:30:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.getElementById("main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+document.body.appendChild(newHeader);
+newHeader.textContent = `<h1>"ivian is the champion"</h1>`
+
